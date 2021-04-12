4ee673b4479d54aa1420df0901757fb357e71be2
gabifioranelli <gabifioranelli@gmail.com>
2021-04-12 14:29:04 -0300

    first commit

---
+++ src/die.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./die.css";
+
+class Die extends Component {
+  render() {
+    return (
+      <i 
+        className={`Die fas fa-dice-${this.props.face} 
+        ${
+          this.props.rolling && 'rolling'
+        }`} 
+      />
+    );
+  }
+}
+export default Die;
+  
